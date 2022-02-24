7ff3e989c96d972cea05766010d7b5c25cbda39d
Arcmazter <aronlex@gmail.com>
2022-02-24 11:05:03 +0000

    Orb fixed and working

--- Assets/Scripts/DiamondDash.cs
+++ Assets/Scripts/DiamondDash.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using AGDDPlatformer;
 
-public class OrbDash : MonoBehaviour
+public class DiamondDash : MonoBehaviour
 {
-
     PlayerController player;
     public float dashSpeed = 2;
     bool isDashing = false;
@@ -21,17 +20,19 @@ public class OrbDash : MonoBehaviour
         {
             player.inOrbRange = true;
             player.dashDirection = (transform.position - player.transform.position).normalized;
+            if (Vector2.Distance(transform.position, player.transform.position) <= 1)
+            {
+                player.dashDirection = (Vector2.up);
+            }
             player.dashSpeed = dashSpeed * player.dashSpeedOriginal;
         }
         if (player.isDashing)
             isDashing = true;
     }
 
-
     private void OnTriggerExit2D(Collider2D collision)
     {
         isDashing = false;
         player.inOrbRange = false;
     }
-
 }
